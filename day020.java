f869400c4d69582810f533c0cd531433b67973ec
IMRAN <88793320+Imranhub786@users.noreply.github.com>
2024-01-21 19:13:29 +0530

    day020.java

---
+++ day020.java
@@ -0,0 +1,73 @@
+/*
+Wave Array
+************************************
+Problem Description
+ 
+ 
+
+Given an array of integers A, sort the array into a wave-like array and return it. 
+In other words, arrange the elements into a sequence such that
+
+a1 >= a2 <= a3 >= a4 <= a5..... 
+NOTE: If multiple answers are possible, return the lexicographically smallest one.
+
+
+
+Problem Constraints
+1 <= len(A) <= 106
+
+1 <= A[i] <= 106
+
+
+
+Input Format
+The first argument is an integer array A.
+
+
+
+Output Format
+Return an array arranged in the sequence as described.
+
+
+
+Example Input
+Input 1:
+
+A = [1, 2, 3, 4]
+Input 2:
+
+A = [1, 2]
+
+
+Example Output
+Output 1:
+
+[2, 1, 4, 3]
+Output 2:
+
+[2, 1]
+
+
+Example Explanation
+Explanation 1:
+
+One possible answer : [2, 1, 4, 3]
+Another possible answer : [4, 1, 3, 2]
+First answer is lexicographically smallest. So, return [2, 1, 4, 3].
+Explanation 1:
+
+Only possible answer is [2, 1].
+*/
+public class Solution {
+    public int[] wave(int[] A) {
+        Arrays.sort(A);
+        for(int i=0;i<A.length-1;i++){
+            int temp=A[i];
+            A[i]=A[i+1];
+            A[i+1]=temp;
+            i++;
+        }
+        return A;
+    }
+}
+
